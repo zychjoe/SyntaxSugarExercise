a45de43a40021c1f47cd8070c217b41942b15d5a
Joe Zedd <zychjoe@gmail.com>
2022-02-03 15:16:12 -0500

    initial commit

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine": $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
+        }
+    }
+}
